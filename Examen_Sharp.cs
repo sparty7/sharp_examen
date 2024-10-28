f70d8659ea5bc692f87d5e047b22a9f8b85fc507
sparty7 <magh080306hgtrzga2@cbtis217.edu.mx>
2024-10-28 07:58:50 -0600

    agregando la base del proyecto

---
+++ Examen_Sharp.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC24
+ * Date: 28/10/2024
+ * Time: 07:54 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Examen_Sharp
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
