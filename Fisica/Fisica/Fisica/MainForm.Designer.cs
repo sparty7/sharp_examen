eae8cefa71e6add163c9ced61285eb52b9da4c56
AlanScottKennedy <Gova080521hgtnzla7@cbtis217.edu.mx>
2024-10-30 08:06:38 -0600

    edicion de codigo

--- Fisica/Fisica/Fisica/MainForm.Designer.cs
+++ Fisica/Fisica/Fisica/MainForm.Designer.cs
@@ -36,12 +36,28 @@ namespace Fisica
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 25);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
 			// 
 			// MainForm
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Fisica";
+			this.ClientSize = new System.Drawing.Size(327, 331);
+			this.Controls.Add(this.button1);
 			this.Name = "MainForm";
+			this.Text = "Fisica";
+			this.ResumeLayout(false);
 		}
+		private System.Windows.Forms.Button button1;
 	}
 }
