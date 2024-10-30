098974d1b6ffca29b66dc3dc40d894425c302d86
AlanScottKennedy <Gova080521hgtnzla7@cbtis217.edu.mx>
2024-10-30 07:48:50 -0600

    Colocacion de Fisica

---
+++ Fisica/Fisica/Fisica/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: CC2_PC24
+ * Date: 30/10/2024
+ * Time: 07:40 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Fisica
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
