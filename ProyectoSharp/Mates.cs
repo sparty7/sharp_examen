74ab1c6f330809fcaa73067bbff87393886262ea
sparty7 <magh080306hgtrzga2@cbtis217.edu.mx>
2024-10-30 08:06:26 -0600

    Cambiando la base

---
+++ ProyectoSharp/Mates.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: alxax
+ * Date: 30/10/2024
+ * Time: 07:57 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ProyectoSharp
+{
+	/// <summary>
+	/// Description of Mates.
+	/// </summary>
+	public partial class Mates : Form
+	{
+		public Mates()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
