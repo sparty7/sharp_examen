2be5f2f54c9cf3d3b145579a8cc00c4518315bb2
IchagGG <goba080418hgtrtna4@cbtis217.edu.mx>
2024-10-31 10:31:26 -0600

    agregando la nueva base

--- ProyectoSharp/humanidades.cs
+++ ProyectoSharp/humanidades.cs
@@ -1,24 +1,23 @@
 ﻿/*
  * Created by SharpDevelop.
- * User: CC2_PC24
- * Date: 28/10/2024
- * Time: 07:54 a. m.
+ * User: alxax
+ * Date: 30/10/2024
+ * Time: 07:58 a. m.
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 
-namespace Examen_Sharp
+namespace ProyectoSharp
 {
 	/// <summary>
-	/// Description of MainForm.
+	/// Description of humanidades.
 	/// </summary>
-	public partial class MainForm : Form
+	public partial class humanidades : Form
 	{
-		public MainForm()
+		public humanidades()
 		{
 			//
 			// The InitializeComponent() call is required for Windows Forms designer support.
